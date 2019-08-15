2e68d9bde29529d64e7c8e3f2b8b460798103fc7
Julian Saadi <jsaadi@mcti.local>
2019-08-15 09:56:55 -0300

    upload del build

---
+++ build/precache-manifest.63519777422b1a1cc921cda80a9623e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ee2e271e24883468a03c2a9a0238bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3886237c8dd3a1ee6bed",
+    "url": "/static/css/main.521f7547.chunk.css"
+  },
+  {
+    "revision": "f2c27f62b62d6c500e13",
+    "url": "/static/js/2.8c25d828.chunk.js"
+  },
+  {
+    "revision": "3886237c8dd3a1ee6bed",
+    "url": "/static/js/main.8ee21259.chunk.js"
+  },
+  {
+    "revision": "fb7953669234c2691588",
+    "url": "/static/js/runtime~main.8519feab.js"
+  }
+]);
